d29afd291085caee16d6eabee6ad0e5f7f1aa864
Nate Mortensen <nate@clearvoice.com>
2018-07-25 11:54:05 -0400

    Basic layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,15 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
+      <img src="~/assets/pickles.svg" class="logo">
       <h1 class="title">
-        the-big-dill
+        The Big Dill
       </h1>
       <h2 class="subtitle">
         A Tasty, Punny JAMStack demo
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+        <nuxt-link to="/menu" class="button--green">Peruse our Menu</nuxt-link>
       </div>
     </div>
   </section>
@@ -32,13 +25,17 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 .container {
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
+}
+
+.logo {
+  max-width: 40%;
 }
 
 .title {
---
+++ pages/menu.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="container">
+    <div class="menu">
+      <nuxt-link class="back" to="/">&larr; Back</nuxt-link>
+      <h2 class="menu__title">Dishes to pickle your fancy:</h2>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style lang="scss">
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.menu {
+  position: relative;
+  display: block;
+  text-align: left;
+  background: antiquewhite;
+  width: 80%;
+  min-height: 50vh;
+  padding: 2rem;
+}
+
+.back {
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: translateY(-115%);
+  display: inline-block;
+  border-bottom: 1px solid currentColor;
+  padding-bottom: 5px;
+  color: black;
+  text-decoration: none;
+  font-weight: bold;
+}
+</style>
+
